75cd014a8709593486e839f55cc35fad138c1aa1
Kuba Vejrazka <jakvejr@gmail.com>
2022-05-05 23:14:51 +0200

    Added image resizing and edited charset

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 # This is the main project file.
+from cairo import Antialias
 from ImageGenerator import ImageGenerator
-from PIL import Image
+from PIL import Image, ImageOps
 from pathlib import Path
 
 
@@ -8,15 +9,19 @@ class Main:
 
     # specifying path to image
     cwd = str(Path(__file__).parent.absolute())
-
-    global img
+    maxSize = (150, 150)
 
     try:
-
         imgPath = cwd + "/image.jpg"
         print(imgPath)
 
         img = Image.open(imgPath)
+
+        imgTemp = img
+
+        img.thumbnail(maxSize, Image.ANTIALIAS)
+        img = ImageOps.exif_transpose(imgTemp)
+        del imgTemp
         imgC = img.load()
 
         ImageGenerator.GetTxt(img.width, img.height, imgC)
