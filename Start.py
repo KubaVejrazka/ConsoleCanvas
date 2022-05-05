77bfbf89f93b71b30cff3c74aaa6d14ca5d6fdfa
Kuba Vejražka <49091814+KubaVejrazka@users.noreply.github.com>
2022-05-06 00:05:56 +0200

    Renamed Main.py to Start.py, made small edits.
    Removed unnecessary imports and added comments. Also removed a print() I used for debugging.

--- Start.py
+++ Start.py
@@ -1,5 +1,5 @@
 # This is the main project file.
-from cairo import Antialias
+
 from ImageGenerator import ImageGenerator
 from PIL import Image, ImageOps
 from pathlib import Path
@@ -9,21 +9,24 @@ class Main:
 
     # specifying path to image
     cwd = str(Path(__file__).parent.absolute())
+
+    # specifying maximum width/length
     maxSize = (150, 150)
 
     try:
         imgPath = cwd + "/image.jpg"
-        print(imgPath)
-
         img = Image.open(imgPath)
 
         imgTemp = img
 
         img.thumbnail(maxSize, Image.ANTIALIAS)
         img = ImageOps.exif_transpose(imgTemp)
+
         del imgTemp
+
         imgC = img.load()
 
+        # start ImageGenerator.GetTxt with selected image
         ImageGenerator.GetTxt(img.width, img.height, imgC)
 
     # in case "image.jpg" doesn't exist:
